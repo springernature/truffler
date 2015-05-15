60d3b4649b26451f50cd22ae5a8116d8f8e8c6d1
Rowan Manning <info@rowanmanning.com>
2015-05-15 15:57:51 +0100

    Add some basic integration tests

--- test/integration/setup.js
+++ test/integration/setup.js
@@ -2,6 +2,20 @@
 /* global before */
 'use strict';
 
-before(function () {
-	// Test setup will go here
+var startWebsite = require('./mock/website');
+
+before(function (done) {
+	var self = this;
+
+	self.websitePort = process.env.PORT || 6540;
+	self.websiteAddress = 'http://localhost:' + self.websitePort;
+
+	startWebsite(self.websitePort, function (error, website) {
+		if (error) {
+			done(error);
+		}
+		self.website = website;
+		done();
+	});
+
 });
---
+++ test/integration/tests.js
@@ -0,0 +1,94 @@
+/* jshint maxstatements: false, maxlen: false */
+/* global after, before, describe, it */
+'use strict';
+
+var assert = require('proclaim');
+var truffler = require('../..');
+
+describe('Truffler Example Application', function () {
+	var error, test, exit;
+
+	before(function (done) {
+		var options = {
+			page: {
+				headers: {
+					Foo: 'bar',
+					Cookie: 'foo=bar'
+				}
+			},
+			testFunction: function (browser, page, completeTest) {
+				page.evaluate(
+					function () {
+						/* global document */
+						return {
+							title: document.title,
+							body: document.body.textContent
+						};
+					},
+					function (result) {
+						completeTest(null, result);
+					}
+				);
+			}
+		};
+		truffler(options, function (trufflerError, trufflerTest, trufflerExit) {
+			error = trufflerError;
+			test = trufflerTest;
+			exit = trufflerExit;
+			done();
+		});
+	});
+
+	after(function () {
+		exit();
+	});
+
+	it('Should not error', function () {
+		assert.isNull(error);
+	});
+
+	describe('with basic HTML', function () {
+
+		it('should report the title of a requested page', function (done) {
+			test(this.websiteAddress + '/', function (error, result) {
+				assert.isNull(error);
+				assert.strictEqual(result.title, 'Index Page');
+				done();
+			});
+		});
+
+		it('should report the body text of the requested page', function (done) {
+			test(this.websiteAddress + '/', function (error, result) {
+				assert.isNull(error);
+				assert.strictEqual(result.body.trim(), 'Hello World!');
+				done();
+			});
+		});
+
+	});
+
+	describe('with a method', function () {
+
+		it('send the expected headers to the requested page', function (done) {
+			test(this.websiteAddress + '/header-dump', function (error, result) {
+				assert.isNull(error);
+				assert.strictEqual(result.body.trim(), 'Cookie: foo=bar\nFoo: bar');
+				done();
+			});
+		});
+
+	});
+
+	describe('with headers', function () {
+
+		it('send the expected headers to the requested page', function (done) {
+			test(this.websiteAddress + '/header-dump', function (error, result) {
+				assert.isNull(error);
+				assert.strictEqual(result.body.trim(), 'Cookie: foo=bar\nFoo: bar');
+				done();
+			});
+		});
+
+	});
+
+});
