9d449d6df4746742f927733b1e23ec6c0cb67627
Jose Bolos <joseluisbolos@gmail.com>
2017-02-23 17:12:48 +0000

    Extract the options to a variable
    Makes the code easier to read but most importantly prevents a
    linting error when upgrading to jshint 3 due to the misaligned
    comment.

--- example/multiple/index.js
+++ example/multiple/index.js
@@ -2,20 +2,17 @@
 
 var async = require('async');
 var truffler = require('../..');
-
-// Create a test instance with some default options
-var test = truffler({
-
+var options = {
 	// Log what's happening to the console
 	log: {
 		debug: console.log.bind(console),
 		error: console.error.bind(console),
 		info: console.log.bind(console)
 	}
+};
 
-// The test function which will get run on URLs
-}, function(browser, page, options, done) {
-
+// Create a test instance with some default options
+var test = truffler(options, function(browser, page, options, done) {
 	// Evaluate the page, extract the title, and callback
 	page.evaluate(
 		function() {
@@ -26,19 +23,16 @@ var test = truffler({
 			done(null, result);
 		}
 	);
-
 });
 
 // Use the async library to run multiple tests in series
 // https://github.com/caolan/async
 async.series({
-
 	// Test the Nature home page
 	home: test.run.bind(test, 'http://nature.com/'),
 
 	// Test the Nature Plants home page
 	plants: test.run.bind(test, 'http://nature.com/nplants/')
-
 }, function(error, results) {
 	if (error) {
 		return console.error(error.message);
